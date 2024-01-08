4af2c9f076d7e1a3b3822564e1be497378b51ca0
Andre Sionek <andresionek91@gmail.com>
2024-01-08 12:14:51 +0000

    feat: initial commit

--- src/stack.py
+++ src/stack.py
@@ -1,6 +1,7 @@
 import aws_cdk as cdk
 
 from constructs import Construct
+from aws_cdk import aws_s3 as s3
 
 
 class MyStack(cdk.Stack):
@@ -9,3 +10,7 @@ class MyStack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        s3.Bucket(
+            scope=self,
+            id="Bucket",
+        )
